4fc40bac6b6d81ffca4206deb37959fa263d55a9
gmzi <gmzieres@gmail.com>
2023-05-16 19:24:06 -0400

    add global components on personal layout

---
+++ components/pages/home/HomePage.tsx
@@ -0,0 +1,21 @@
+export function HomePage(){
+    return (
+        <>
+        <section>
+            <h2>Section About</h2>
+        </section>
+        <section>
+            <h2>Section Shows</h2>
+        </section>
+        <section>
+            <h2>Section Reviews</h2>
+        </section>
+        <section>
+            <h2>Section Courses</h2>
+        </section>
+        <section>
+            <h2>Section Podcast</h2>
+        </section>
+        </>
+    )
+}
