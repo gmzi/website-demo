00637d33ab1264535d8f40e93361609e6246085d
gmzi <gmzieres@gmail.com>
2023-05-26 17:49:34 -0400

    text content cycle on about

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -1,17 +1,16 @@
-import { Header } from "@/components/shared/Header"
-import { getData } from "@/lib/getData"
+import parse from 'html-react-parser'
 
 interface Data {
     image_url: string;
     content_html: string;
 }
 
-export function HomePage({image_url, content_html}: Data){    
+export function HomePage({image_url, content_html}: Data){   
     return (
         <main>
             <section>
-                <img src={image_url}/>
-                {content_html}
+                <img src={image_url} style={{width: '20%', height: 'auto'}}/>
+                {parse(content_html)}
             </section>
         </main>
     )
