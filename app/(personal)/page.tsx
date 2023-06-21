140fef4ecb3985f5af8bfca1d7195605a0193e03
gmzi <gmzieres@gmail.com>
2023-06-21 16:31:00 -0400

    move home page logic to /personal/page

--- app/(personal)/page.tsx
+++ app/(personal)/page.tsx
@@ -1,10 +1,41 @@
+import type { Metadata } from "next"
 import { HomePage } from "@/components/pages/home/HomePage"
+import Image from 'next/image'
+import { getData } from "@/lib/getData"
+import parse from 'html-react-parser'
 
-export default async function IndexRoute() {
+export const metadata: Metadata = {
+    title: 'About',
+    description: 'dramaturgo, director, docente y actor',
+}
+
+export default async function AboutPage() {
+    const data = await getData("about");
+    const text = parse(data?.content_html) || '';
+    const imageUrl = data?.image_url || '';
+
     return (
-        <>
-            {/* @ts-expect-error Server Component */}
-            <HomePage/>
-        </>
+        <section className='sectionAbout'>
+            <h1>Bio</h1>
+            <div>
+                <div className='imgContainer'>
+                    <Image
+                        src={imageUrl}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{
+                            width: '30%', 
+                            height: 'auto', 
+                            borderRadius: '5px', 
+                            float: 'left',
+                            marginRight: '1em',
+                        }}
+                        alt="Picture of the author"
+                    />
+                </div>
+                {text}
+            </div>
+        </section>
     )
 }
