e033591bede95d57a0506fad5113d6ff4773b1a0
gmzi <gmzieres@gmail.com>
2023-09-15 17:59:13 -0400

    add allowedRedirectOrigins

--- app/(editor)/editor/layout.tsx
+++ app/(editor)/editor/layout.tsx
@@ -7,7 +7,7 @@ interface EditorProps {
 export default async function EditorLayout({ children }: EditorProps) {
 
   return (
-    <ClerkProvider>
+    <ClerkProvider allowedRedirectOrigins={['http://localhost:3000/editor', 'https://website-fer.vercel.app/editor']}>
       <div className="container mx-auto grid items-start gap-10 py-8">
         {children}
       </div>
