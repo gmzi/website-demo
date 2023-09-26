d79b4fe82d15da2af637e58082d07d884abda1ab
gmzi <gmzieres@gmail.com>
2023-09-26 12:49:13 -0400

    disable afterAuth, implement orgRole

--- app/(editor)/editor/page.tsx
+++ app/(editor)/editor/page.tsx
@@ -15,10 +15,13 @@ const AUTHORIZED_USER_ID = process.env.AUTHORIZED_USER_ID;
 
 export default async function EditorMainPage() {
 
-  const user: User | null = await currentUser();
+  // const user: User | null = await currentUser();
+  const {orgRole} = auth();
+
+  console.log(orgRole);
   // const {userId} : {userId: string | null} = auth();
 
-  if (user?.id !== AUTHORIZED_USER_ID) {
+  if (orgRole !== 'admin') {
     return (
       <div>
         <UserButton afterSignOutUrl="/editor" />
@@ -28,6 +31,16 @@ export default async function EditorMainPage() {
 
     )
   }
+  // if (user?.id !== AUTHORIZED_USER_ID) {
+  //   return (
+  //     <div>
+  //       <UserButton afterSignOutUrl="/editor" />
+  //       <SignOutButton />
+  //       <p>you are an unauthorized user to edit this page, please sign out from your account and sign in as an authorized user</p>
+  //     </div>
+
+  //   )
+  // }
 
   return (
     <>
