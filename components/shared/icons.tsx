f33ecd28c5027f9ac3f4a59e958edfb2816bcec7
gmzi <gmzieres@gmail.com>
2023-10-23 16:04:54 -0400

    fix styles:

--- components/shared/icons.tsx
+++ components/shared/icons.tsx
@@ -154,7 +154,7 @@ export function Upload() {
             viewBox="0 0 24 24" 
             fill="none" 
             stroke="currentColor" 
-            strokeWidth="2" 
+            strokeWidth="1" 
             strokeLinecap="round" 
             strokeLinejoin="round" 
             className="lucide lucide-file-up">
