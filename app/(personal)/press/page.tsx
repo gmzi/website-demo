7d745cd4555fe4ec46ab654821a8dd482d9cd28f
gmzi <gmzieres@gmail.com>
2023-10-31 12:09:21 -0400

    style written_press articles

--- app/(personal)/press/page.tsx
+++ app/(personal)/press/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { getData } from "@/lib/getData";
+import { getRemoteOrLocalData } from "@/lib/getRemoteOrLocalData";
 import Image from "next/image";
 import Link from "next/link";
 import type { WrittenPressArticle } from "@/types";
@@ -7,14 +8,15 @@ import type { VideoPressArticle } from "@/types";
 
 export const metadata: Metadata = {
     title: 'Prensa',
-    description: 'articulos de prensa'
+    description: 'articulos de prensa sobre John Doe'
 }
 
 export default async function PressPage() {
 
-    const data = await getData('press');
+    // const data = await getData('press');
+    const data = await getRemoteOrLocalData('press');
 
-    const hero_image_url: string = data.hero_image_url;
+    const image_1_url: string = data.image_1_url;
 
     const written_articles: WrittenPressArticle[] = data.written_press;
 
@@ -25,47 +27,46 @@ export default async function PressPage() {
     const video_uniqueShows: string[] = [...new Set(video_articles.map((item: { show: string }) => item.show))]
 
     return (
-        <section>
+        <section className="press">
             <h1>Prensa</h1>
-            <Image
-                src={hero_image_url}
-                alt="press-image"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{
-                    width: '70%',
-                    height: 'auto',
-                    borderRadius: '5px',
-                    marginBottom: '.5em'
-                }}
-            />
+            <div className="imgContainer">
+                <Image
+                    className="defaultImgStyle"
+                    src={image_1_url}
+                    alt="press-image"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                />
+            </div>
+
             <h2>Artículos</h2>
             {written_uniqueShows.map(show => (
-                <div key={show}>
-                    <h3>{show}</h3>
-                    {written_articles
-                        .filter((item: { show: string }) => item.show === show)
-                        .map((item, index) => (
-                            <Link key={index} href={item.article_url}>
-                                <div className="press-card">
-                                    <div className="press-card-header">
-                                        <h4>{item.veredict}</h4>
-                                    </div>
-                                    <div>
-                                        <p>{item.quote}</p>
-                                        <div>
-                                            <span className="journalist">{item.journalist}</span>
-                                        </div>
-                                        <div>
-                                            <span className="media-organization">{item.media_organization}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Link>
-                        ))
-                    }
-                </div>
+                <ul key={show} className="years-list">
+                    <li className="">
+                        <h3 className="show-item">{show}</h3>
+                        <ul className="tours-list">
+                            {written_articles
+                                .filter((item: { show: string }) => item.show === show)
+                                .map((item, index) => (
+                                    <Link key={index} href={item.article_url} target="_blank" className="press-link">
+                                        <li className="">
+                                            <blockquote className="review">
+                                                <h4>{item.veredict}</h4>
+                                                <p>{item.quote}</p>
+                                                <cite>
+                                                    <div className="journalist">{item.journalist}</div>
+                                                    <div className="media-organization">{item.media_organization}</div>
+                                                </cite>
+                                            </blockquote>
+                                        </li>
+                                    </Link>
+                                ))
+                            }
+
+                        </ul>
+                    </li>
+                </ul>
             ))}
 
             <h2>Entrevistas</h2>
