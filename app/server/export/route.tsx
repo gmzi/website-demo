e747e3568b4437701db35f04e152f24bfc64eb1b
gmzi <gmzieres@gmail.com>
2024-05-10 16:35:59 -0400

    add route to download a collection

--- app/server/export/route.tsx
+++ app/server/export/route.tsx
@@ -1,6 +1,6 @@
 // Export a mongoDB collection to a json file. 
 
-//http://localhost:3000/server/export?collection_name=dev-collection
+//http://localhost:3000/server/export?database_name=some&collection_name=dev-collection
 
 import { NextRequest, NextResponse } from 'next/server';
 import { exportCollection } from '@/lib/mongo';
@@ -13,23 +13,31 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url)
+
+    const DATABASE_NAME = searchParams.get('database_name')
     const COLLECTION_NAME = searchParams.get('collection_name')
+    
     try {
         if (isProd){
             return NextResponse.json({ message: `backup not allowed in prod` });
         }
 
+        if (!DATABASE_NAME){
+            return NextResponse.json({message: "missing database name"})
+        }
         if (!COLLECTION_NAME){
             return NextResponse.json({message: "missing collection name"})
         }
+        
 
         const currentDate = new Date();
-        // const about = await getDataFromProd("about");
-        // const shows = await getDataFromProd("shows");
-        const collection = await exportCollection(COLLECTION_NAME)
+        
+        const {collection, collections} = await exportCollection(DATABASE_NAME, COLLECTION_NAME)
 
+        if (!collection){
+            return NextResponse.json({message: "collection not existing or empty"})
+        }
         const isEmpty = Object.keys(collection).length === 0;
-
         if (isEmpty){
             return NextResponse.json({message: "collection not existing or empty"})
         }
@@ -51,7 +59,11 @@ export async function GET(req: Request) {
                 console.log("local.json file with data from prod database has been saved.");
             }
         });
-        return NextResponse.json({ message: `export successfull at ${currentDate}. Find exported-${COLLECTION_NAME}-local.json in root folder`});
+
+        return NextResponse.json({ 
+            message: `export successfull at ${currentDate}. Find exported-${COLLECTION_NAME}-local.json in root folder.`,
+            collections: collections
+        });
     } catch (e) {
         console.log(e)
         return NextResponse.json({ error: `${e}` })
